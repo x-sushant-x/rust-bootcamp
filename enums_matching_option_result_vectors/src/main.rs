75ca2ef8863d942f943e31da152f2578ba211ceb
Sushant <sushant.dhiman9812@gmail.com>
2024-10-07 08:26:20 +0530

    option, result and vectors

--- enums_matching_option_result_vectors/src/main.rs
+++ enums_matching_option_result_vectors/src/main.rs
@@ -38,4 +38,50 @@ fn main() {
 
     let serialized_command = replace.serialize();
     println!("serialized_command: {serialized_command}");
+
+    let username = get_username(1);
+    match &username {
+        Some(name) => println!("Hi {name}"),
+        None => println!("No user found")
+    }
+
+    if let Some(name) = username {
+        println!("Hi {name}")
+    }
+
+    let mut v = Vec::new();
+    v.push("Hi");
+    v.push("Sushant");
+    v.push("Here");
+
+    let mut v2 = vec![1, 2, 3];
+
+    if let Some(el) = v2.get(1) {
+        println!("{el}")
+    }
+
+    for e in v {
+        v2.push(e.parse::<i32>().unwrap());
+    }
+}
+
+
+fn get_username(user_id: i32) -> Option<String> {
+    // if user_id == 1 {
+    //     Some(String::from("Sushant"))
+    // } else {
+    //     None
+    // }
+
+    let query = format!("SELECT username FROM users WHERE id = {user_id}");
+    let db_result = query_db(query);
+    db_result.ok()
+}
+
+fn query_db(query: String) -> Result<String, String> {
+    if query.is_empty() {
+        Err(String::from("Can not use empty query."))
+    } else {
+        Ok(String::from("Sushant"))
+    }
 }
